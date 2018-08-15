6c906702fcda8e2e1ddc4c8048d15848ffcabe7c
Mariya Gachkova <38293003+mariagachkova@users.noreply.github.com>
2018-08-15 16:30:35 +0300

    Add existing files to repository
    The code is not tested.

---
+++ db/upgrade.php
@@ -0,0 +1,64 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Upgrade scripts for course format "Levels"
+ *
+ * @package    format_levels
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Upgrade script for format_levels
+ *
+ * @param int $oldversion the version we are upgrading from
+ * @return bool result
+ */
+function xmldb_format_levels_upgrade($oldversion) {
+    global $CFG, $DB;
+
+    require_once($CFG->dirroot . '/course/format/levels/db/upgradelib.php');
+
+    if ($oldversion < 2017020200) {
+
+        // Remove 'numsections' option and hide or delete orphaned sections.
+        format_levels_upgrade_remove_numsections();
+
+        upgrade_plugin_savepoint(true, 2017020200, 'format', 'levels');
+    }
+
+    // Automatically generated Moodle v3.3.0 release upgrade line.
+    // Put any upgrade step following this.
+
+    // Automatically generated Moodle v3.4.0 release upgrade line.
+    // Put any upgrade step following this.
+
+    if ($oldversion < 2018030900) {
+
+        // During upgrade to Moodle 3.3 it could happen that general section (section 0) became 'invisible'.
+        // It should always be visible.
+        $DB->execute("UPDATE {course_sections} SET visible=1 WHERE visible=0 AND section=0 AND course IN
+        (SELECT id FROM {course} WHERE format=?)", ['levels']);
+
+        upgrade_plugin_savepoint(true, 2018030900, 'format', 'levels');
+    }
+
+    // Automatically generated Moodle v3.5.0 release upgrade line.
+    // Put any upgrade step following this.
+
+    return true;
+}
---
+++ lang/en/format_levels.php
@@ -0,0 +1,61 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'format_levels', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   format_levels
+ */
+
+$string['addsections'] = 'Add levels';
+$string['currentsection'] = 'This level';
+$string['editsection'] = 'Edit level';
+$string['editsectionname'] = 'Edit level name';
+$string['deletesection'] = 'Delete level';
+$string['newsectionname'] = 'New name for level {$a}';
+$string['sectionname'] = 'Level';
+$string['pluginname'] = 'Levels format';
+$string['section0name'] = 'General';
+$string['page-course-view-levels'] = 'Any course main page in levels format';
+$string['page-course-view-levels-x'] = 'Any course page in levels format';
+$string['hidefromothers'] = 'Hide level';
+$string['showfromothers'] = 'Show level';
+$string['privacy:metadata'] = 'The Levels format plugin does not store any personal data.';
+$string['showdefaultsectionname'] = 'Show the default levels name';
+$string['showdefaultsectionname_help'] = 'If no name is set for the section will not show anything.<br>
+By definition an unnamed topic is displayed as <strong>Level [N]</strong>.';
+$string['yes'] = 'Yes';
+$string['no'] = 'No';
+$string['sectionposition'] = 'Section zero position';
+$string['sectionposition_help'] = 'The section 0 will appear together the visible section.<br><br>
+<strong>Above the list buttons</strong><br>Use this option if you want to add some text or resource before the buttons list.
+<i>Example: Define a picture to illustrate the course.</i><br><br><strong>Below the visible section</strong><br>
+Use this option if you want to add some text or resource after the visible section.
+<i>Example: Resources or links to be displayed regardless of the visible section.</i><br><br>';
+$string['above'] = 'Above the list buttons';
+$string['below'] = 'Below the visible section';
+$string['colorhighlight'] = 'Color of the highlight level';
+$string['colorhighlight_help'] = 'The current level is the section marked with highlight.<br>Define a color in hexadecimal.
+<i>Example: #fab747</i><br>If you want to use the default color, leave empty.';
+$string['colorcurrent'] = 'Color of the current level';
+$string['colorcurrent_help'] = 'The current level is the selected section.<br>Define a color in hexadecimal.
+<i>Example: #747fab</i><br>If you want to use the default color, leave empty.';
+$string['editing'] = 'The buttons are disabled while the edit mode is active.';
+$string['sectiontype'] = 'List style';
+$string['numeric'] = 'Numeric';
+$string['roman'] = 'Roman numerals';
+$string['alphabet'] = 'Alphabet';
